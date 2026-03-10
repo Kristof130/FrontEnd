aca4c39cba0db709e5314d73898aafb98efb55ed
Kristof130 <pappkristof2006@gmail.com>
2026-03-10 09:51:09 +0100

    Berlesrazor

--- GameStore.Frontend/Clients/BerloClient.cs
+++ GameStore.Frontend/Clients/BerloClient.cs
@@ -4,15 +4,15 @@ namespace GameStore.Frontend.Clients;
 
 public class BerloClient(HttpClient httpClient)
 {    
-    public async Task<BerloSummary[]> GetGamesAsync() => await httpClient.GetFromJsonAsync<BerloSummary[]>("berlo") ?? [];
+    public async Task<BerloSummary[]> GetBerloAsync() => await httpClient.GetFromJsonAsync<BerloSummary[]>("berlo") ?? [];
 
-    public async Task AddGameAsync(GameDetails game) => await httpClient.PostAsJsonAsync("berlo", game);
+    public async Task AddBerloAsync(BerloDetails game) => await httpClient.PostAsJsonAsync("berlo", game);
 
-    public async Task<GameDetails> GetGameAsync(int id) => 
-        await httpClient.GetFromJsonAsync<GameDetails>($"berlo/{id}") 
+    public async Task<BerloDetails> GetBerloAsync(int id) => 
+        await httpClient.GetFromJsonAsync<BerloDetails>($"berlo/{id}") 
         ?? throw new InvalidOperationException($"Game with ID {id} not found.");
-    public async Task UpdateGameAsync(GameDetails updatedGame) =>    
-        await httpClient.PutAsJsonAsync($"berlo/{updatedGame.Id}", updatedGame);
-    public async Task DeleteGameAsync(int id) =>    
+    /*public async Task UpdateBerloAsync(GameDetails updatedGame) =>    
+        await httpClient.PutAsJsonAsync($"berlo/{updatedGame.Id}", updatedGame);*/
+    public async Task DeleteBerloAsync(int id) =>    
         await httpClient.DeleteAsync($"berlo/{id}");    
 }
